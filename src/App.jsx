ad8b34ef8ca7f68d3f0f50e76995db684384a4a4
haitam <haitam@gmail.com>
2026-01-11 11:55:38 +0100

    update some lignes in the index

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import About from './Components/Pages/About/About'
-import Home from './Components/Pages/Home/Home'
+import About from "./Components/Pages/About/About";
+import Home from "./Components/Pages/Home/Home";
+import Menu from "./Components/Pages/Menu/Menu";
+// import Gallery from "./Components/Pages/Gallery/"
 
 export default function App() {
   return (
-	<div>
-		<Home />
-		<About />
-	</div>
-  )
+    <div>
+      <Home />
+      <About />
+	  <Menu />
+    </div>
+  );
 }
