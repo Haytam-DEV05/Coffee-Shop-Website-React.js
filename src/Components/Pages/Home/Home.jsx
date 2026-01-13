5a265d1532b9e5c7a2c316f58913269dfdb7643f
haitam <haitam@gmail.com>
2026-01-13 23:17:57 +0100

    make the nav-bare responsive

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -3,6 +3,16 @@ import { FiCoffee } from "react-icons/fi";
 import { FaRegUserCircle } from "react-icons/fa";
 
 export default function Home() {
+  const links = [
+    { id: 1, path: "home", name: "Home" },
+    { id: 2, path: "about", name: "about" },
+    { id: 3, path: "menu", name: "Menu" },
+    { id: 4, path: "gallery", name: "Gallery" },
+    { id: 5, path: "reviews", name: "Reviews" },
+    { id: 5, path: "contact", name: "Contact" },
+    { id: 5, path: "blogs", name: "Blogs" },
+  ];
+
   return (
     <header id="header">
       <nav>
@@ -10,34 +20,25 @@ export default function Home() {
           <FiCoffee />
           <h3>Coffee</h3>
         </div>
-        <ul>
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#menu">Menu</a>
-          </li>
-          <li>
-            <a href="#gallery">Gallery</a>
-          </li>
-          <li>
-            <a href="#reviews">Reviews</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-          <li>
-            <a href="#blogs">Blogs</a>
-          </li>
-          <li>
-            <a href="">
-              <FaRegUserCircle />
-            </a>
-          </li>
-        </ul>
+        <div className="right-menu">
+          <ul>
+            {links.map((link) => {
+              return (
+                <li>
+                  <a href={link.path}>{link.name}</a>
+                </li>
+              );
+            })}
+          </ul>
+          <button>
+            <FaRegUserCircle />
+          </button>
+        </div>
+
+        {/* MOBILE MENU */}
+        {/* <div className="right-menu">
+          <div className="menu-navigation"></div>
+        </div> */}
       </nav>
 
       <div className="home" id="home">
